73ad61d5c07d88fc43b91408fe6d9ed955648d8e
Alfie Freeman <runticle@gmail.com>
2023-02-13 14:11:29 +0000

    bubbles look nice

--- src/components/Bubble/index.js
+++ src/components/Bubble/index.js
@@ -5,18 +5,33 @@ import styled from "styled-components"
 
 const BubbleStyle = styled.div`
     padding: 20px;
-    background-color: blue;
-    opacity: 0.5;
+    opacity: 0.7;
     position: absolute;
     transition: linear 10s;
     border-radius: 50%;
     color: white;
     min-height: 100px;
     min-width: 100px;
+    max-width: 150px;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
+    background: radial-gradient(circle at bottom, #81e8f6, #76deef 10%, #055194 80%, #062745 100%);
+
+    &:before {
+        content: "";
+        position: absolute;
+        top: 1%;
+        left: 6%;
+
+        width: 95%;
+        height: 95%;
+        border-radius: 100%;
+        background: radial-gradient(circle at top, white, rgba(255, 255, 255, 0.1) 58%);
+        filter: blur(5px);
+        z-index: 2; 
+    }
 `
 
 export default function Bubble({ idea, onClickHandler }) {
