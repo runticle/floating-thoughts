e2ea7bbc7a826c870b630a0ecaa99c9e1b623843
Alfie Freeman <runticle@gmail.com>
2023-02-13 18:11:33 +0000

    new bubble component styled. looks terrible I don't know what to do I'm so new at design lol

--- src/components/NewBubble/index.js
+++ src/components/NewBubble/index.js
@@ -2,7 +2,61 @@ import { useState } from "react"
 import styled from "styled-components"
 
 const NewBubbleStyle = styled.div`
+  position: absolute;
+  bottom: 0;
+  height: 15vh;
+  left: 0;
+  right: 0;
+
+  padding: 20px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  > * {
+    font-family: calm;
+    font-size: 20px;
+  }
   
+
+  line-height: 1rem;
+
+  input {
+    width: 100vw;
+    max-width: 350px;
+    background: white;
+    padding-left: 10px;
+    border-radius: 5px;
+    border: 1px solid #87CDF6;
+    height: 30px;
+    color: #87CDF6;
+
+    &::placeholder {
+        color: #87CDF6;
+    }
+
+    &:active, &:focus {
+
+    }
+  }
+    button {
+        width: 100vw;
+        max-width: 150px;
+
+        background: #87CDF6;
+        border-radius: 5px;
+        border: 1px solid #87CDF6;
+        height: 30px;
+
+        color: white;
+    }
+
+  border-top: 1px solid #8CE68C;
+  background: radial-gradient(ellipse at 50% 0%, #8CE68C, #ABF1BC 20%, #CFFFF6 60%, #AEE7F8 70%, #87CDF6 100%);
+
 `
 
 export default function NewBubble({ onSubmit }) {
@@ -19,10 +73,7 @@ export default function NewBubble({ onSubmit }) {
 
     return (
         <NewBubbleStyle>
-            <p>
-                Add thought
-            </p>
-            <input value={thought} onChange={handleChange} placeholder="Enter" />.
+            <input value={thought} onChange={handleChange} placeholder="What are you thinking?" />
             <button type="submit" onClick={handleSubmit}>
                 Release
             </button>
